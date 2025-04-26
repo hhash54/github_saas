e244689d56d671f1e1be6dfc3c1ebc76d8fa6e69
hhash54 <hhashem2002@gmail.com>
2025-04-26 16:15:57 -0400

    showing commits on dashboard ui

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -38,5 +38,10 @@ export const projectRouter=createTRPCRouter({
                 deletedAt:null
             }
         })
-    })
+    }),
+    getCommits: protectedProcedure.input(z.object({
+      projectId: z.string(),
+    })).query(async ({ ctx, input }) => {
+      return await ctx.db.commit.findMany({where: { projectId: input.projectId },});
+    })  
 })
