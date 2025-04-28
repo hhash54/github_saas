89befe1fbf3124a36972f3b46a6bd709d1e9d043
hhash54 <hhashem2002@gmail.com>
2025-04-28 17:00:36 -0400

    set up ui for rag

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import MDEditor from '@uiw/react-md-editor'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Textarea } from '@/components/ui/textarea'
 import useProject from '@/hooks/use-project'
@@ -19,12 +19,15 @@ const AskQuestionCard = () => {
     const [answer, setAnswer] = React.useState('')
 
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        setAnswer('')
+        setFilesReferences([])
         e.preventDefault()
         if (!project?.id) return
         setLoading(true)
-        setOpen(true)
     
         const { output, filesReferences } = await askQuestion(question, project.id)
+        setOpen(true)
+
         setFilesReferences(filesReferences)
         for await (const delta of readStreamableValue(output)) {
             if (delta) {
@@ -37,17 +40,18 @@ const AskQuestionCard = () => {
     return (
       <>
         <Dialog open={open} onOpenChange={setOpen}>
-            <DialogContent>
+            <DialogContent className=' flex'>
             <DialogHeader>
                     <DialogTitle>
                         <img src="/logo.png" alt="dionysus" width={40} height={40} />
                     </DialogTitle>
                 </DialogHeader>
-                {answer}
-                <h1>Files References</h1>
-                {filesReferences.map(file => {
-                return <span>{file.fileName}</span>
-                })}
+                <MDEditor.Markdown source={answer} className='max-w-[70vw] h-full max-h-[40vh] overflow-scroll' />
+
+                    <Button type='button' onClick={() => setOpen(false)}>
+                        Close
+                    </Button>
+
 
             </DialogContent>
       </Dialog>
@@ -63,7 +67,7 @@ const AskQuestionCard = () => {
                 onChange={(e) => setQuestion(e.target.value)}
               />
               <div className="h-4"></div>
-              <Button type='submit'>
+              <Button type='submit' disabled={loading}>
                 Ask Dionysus!
               </Button>
             </form>
