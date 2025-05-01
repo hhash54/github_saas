99cd6faee0e82dff2bde7c8c5b97d2ebe0d5b83f
hhash54 <hhashem2002@gmail.com>
2025-05-01 17:54:39 -0400

    adding meet card pt 3

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -1,13 +1,33 @@
 'use client'
 
+import { Card } from '@/components/ui/card'
+import { useDropzone } from 'react-dropzone'
 import React from 'react'
+import { uploadFile } from '@/lib/firebase'
+
 
 const MeetingCard = () => {
-  const [progress, setProgress] = React.useState(0)
-
-  return (
-    <div>MeetingCard</div>
-  )
-}
-
-export default MeetingCard
+    const [isUploading, setIsUploading] = React.useState(false)
+    const [progress, setProgress] = React.useState(0)
+    const { getRootProps, getInputProps } = useDropzone({
+      accept: {
+        'audio/*': ['.mp3', '.wav', '.m4a'],
+      },
+      multiple: false,
+      maxSize: 50_000_000,
+      onDrop: async acceptedFiles => {
+        console.log(acceptedFiles)
+        const file = acceptedFiles[0]
+        const downloadURL = await uploadFile(file as File, setProgress)
+      }
+      
+    })
+  
+    return (
+      <Card className='col-span-2 flex flex-col items-center justify-center'>
+      </Card>
+    )
+  }
+  
+  export default MeetingCard
+  
