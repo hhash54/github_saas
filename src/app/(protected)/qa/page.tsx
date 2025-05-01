a54dc6b86c9618a3b19454cf2743015291c074e6
hhash54 <hhashem2002@gmail.com>
2025-05-01 15:57:06 -0400

    set up qa pt2

--- src/app/(protected)/qa/page.tsx
+++ src/app/(protected)/qa/page.tsx
@@ -1,11 +1,33 @@
+import { Sheet } from '@/components/ui/sheet'
+import useProject from '@/hooks/use-project'
+import { api } from '@/trpc/react'
 import React from 'react'
+import AskQuestionCard from '../dashboard/ask-question-card'
 
 const QAPage = () => {
+  const { projectId } = useProject()
+  const { data: questions } = api.project.getQuestions.useQuery({ projectId })
+
+  const [questionIndex, setQuestionIndex] = React.useState(0)
+  const question = questions?.[questionIndex]
+
   return (
-    <div>
-      QAPage
-    </div>
+    <Sheet>
+      <AskQuestionCard />
+      <div className="h-4"></div>
+      <h1 className="text-xl font-semibold">Saved Questions</h1>
+      <div className="h-2"></div>
+      <div className="flex flex-col gap-2">
+        {questions?.map((question, index) => {
+          return (
+            <React.Fragment key={question.id}>
+              <SheetTrigger>
+                {/* your trigger UI here */}
+              </SheetTrigger>
+            </React.Fragment>
+          )
+        })}
+      </div>
+    </Sheet>
   )
 }
-
-export default QAPage
