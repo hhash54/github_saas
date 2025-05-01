673dbdb21c494626b3f3d815b759fe5ac29886f2
hhash54 <hhashem2002@gmail.com>
2025-05-01 16:14:10 -0400

    add meeting card pt2

---
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const MeetingCard = () => {
+  const [progress, setProgress] = React.useState(0)
+
+  return (
+    <div>MeetingCard</div>
+  )
+}
+
+export default MeetingCard
