225742c647037359ffd030a25bb423d51cf0e75e
hhash54 <hhashem2002@gmail.com>
2025-05-01 18:27:02 -0400

    connect firebase storage

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -4,6 +4,9 @@ import { Card } from '@/components/ui/card'
 import { useDropzone } from 'react-dropzone'
 import React from 'react'
 import { uploadFile } from '@/lib/firebase'
+import { Presentation, Upload } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
 
 const MeetingCard = () => {
@@ -16,17 +19,50 @@ const MeetingCard = () => {
       multiple: false,
       maxSize: 50_000_000,
       onDrop: async acceptedFiles => {
+        setIsUploading(true)
         console.log(acceptedFiles)
         const file = acceptedFiles[0]
         const downloadURL = await uploadFile(file as File, setProgress)
+        window.alert(downloadURL)
+        setIsUploading(false)
       }
       
     })
   
     return (
-      <Card className='col-span-2 flex flex-col items-center justify-center'>
-      </Card>
-    )
+        <Card className="col-span-2 flex flex-col items-center justify-center p-10" {...getRootProps()}>
+          {!isUploading && (
+            <>
+              <Presentation className="h-10 w-10 animate-bounce" />
+              <h3 className="mt-2 text-sm font-semibold text-gray-900">
+                Create a new meeting
+              </h3>
+              <p className="mt-1 text-center text-sm text-gray-500">
+                Analyse your meeting with Dionysus.
+                <br />
+                Powered by AI.
+              </p>
+              <div className="mt-6">
+                <Button disabled={isUploading}>
+                  <Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+                  Upload Meeting
+                  <input className="hidden" {...getInputProps()} />
+                </Button>
+              </div>
+            </>
+          )}
+          {isUploading && (
+            <div className=''>
+                <CircularProgressbar value={progress} text={`${progress}%`} className='size-20' />
+                <p className='text-sm text-gray-500 text-center'>
+                Uploading your meeting...
+                </p>
+            </div>
+        )}
+
+        </Card>
+      )
+      
   }
   
   export default MeetingCard
